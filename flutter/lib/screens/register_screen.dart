e1632fd0651c1fd127f8ab21fcf6163a0893709e
yoku-2093 <oku_yuya@cyberagent.co.jp>
2025-01-22 18:16:44 +0900

    wip: google auth

--- flutter/lib/screens/register_screen.dart
+++ flutter/lib/screens/register_screen.dart
@@ -1,12 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
 class Register extends StatelessWidget {
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('サインアップ'),
       ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () async {
+            await signInWithGoogle();
+          },
+          child: Text('Googleでサインアップ'),
+        ),
+      )
     );
   }
+}
+
+Future<UserCredential> signInWithGoogle() async {
+  // Trigger the authentication flow
+  final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
+
+  // Obtain the auth details from the request
+  final GoogleSignInAuthentication? googleAuth = await googleUser?.authentication;
+
+  // Create a new credential
+  final credential = GoogleAuthProvider.credential(
+    accessToken: googleAuth?.accessToken,
+    idToken: googleAuth?.idToken,
+  );
+
+  // Once signed in, return the UserCredential
+  return await FirebaseAuth.instance.signInWithCredential(credential);
 }
