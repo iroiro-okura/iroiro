7b42eee2ea235b214edf526fe59990066a7756fb
yoku-2093 <oku_yuya@cyberagent.co.jp>
2025-01-31 00:14:03 +0900

    updateUser

--- flutter/lib/firebase/firestore.dart
+++ flutter/lib/firebase/firestore.dart
@@ -28,6 +28,43 @@ class FirestoreService {
     return null;
   }
 
+  static Future<void> updateUser(model.User user) async {
+    User? authUser = AuthService.auth.currentUser;
+    if (authUser == null) {
+      return;
+    }
+    var existingUserDoc = await db.collection('users').doc(authUser.uid).get();
+    if (!existingUserDoc.exists) {
+      return;
+    }
+    model.User existingUser = model.User.fromSnapshot(authUser.uid, existingUserDoc);
+    // Create a map to hold the updated fields
+    Map<String, dynamic> updatedFields = {};
+
+    // Compare each field and add to the map if different
+    if (user.email != existingUser.email) {
+      updatedFields['email'] = user.email;
+    }
+    if (user.name != existingUser.name) {
+      updatedFields['name'] = user.name;
+    }
+    if (user.age != existingUser.age) {
+      updatedFields['age'] = user.age;
+    }
+    if (user.sex != existingUser.sex) {
+      updatedFields['sex'] = user.sex?.index;
+    }
+    if (user.occupation != existingUser.occupation) {
+      updatedFields['occupation'] = user.occupation;
+    }
+
+    // Update the Firestore document with the changed fields
+    logger.i('Setting user $user');
+    if (updatedFields.isNotEmpty) {
+      await db.collection('users').doc(authUser.uid).update(updatedFields);
+    }
+  }
+
   static Future<void> registerUser() async {
     User? authUser = AuthService.auth.currentUser;
     model.User? user = authUser != null ? await getUser() : null;
